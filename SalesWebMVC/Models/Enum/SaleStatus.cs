e29fa8b6616215b7978ff6d392744774bd1b1f9e
Rayanne Ribeiro <mouraribeiro11@gmail.com>
2023-05-09 15:27:24 -0300

    Others Entities

---
+++ SalesWebMVC/Models/Enum/SaleStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesWebMVC.Models.Enum
+{
+    public enum SaleStatus
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+
+    
+}
---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMVC.Models.Enum;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Amount{ get; set; }
+
+        public SaleStatus status { get; set; }
+
+        public Seller Seller { get; set; }
+
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            this.status = status;
+            Seller = seller;
+        }
+    }
+}
