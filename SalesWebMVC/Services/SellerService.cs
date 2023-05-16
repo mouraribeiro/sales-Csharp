72eed9ae97648af619e69cf44b4dd8958a351cd8
Rayanne Ribeiro <mouraribeiro11@gmail.com>
2023-05-16 11:03:21 -0300

    edit

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,5 +1,7 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -24,13 +26,32 @@ namespace SalesWebMVC.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj =>obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        public void Update(Seller obj) 
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id)) 
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+
+            }
+
+           
         }
     }
 }
